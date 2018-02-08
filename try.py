13669dc12297b1efd492321e1f09e521b0f48905
prasoonpranjal <prasoonpranjal@gmail.com>
2018-02-09 03:55:28 +0530

    Updated DFS (all requred nodes included)

--- try.py
+++ try.py
@@ -2,19 +2,96 @@ import pycparser as pyc
 from sys import argv
 
 def DFS(node):
+	#TypeDef , TypeDecl, Typename etc which had no use haven't been written- we needn't perform DFS on those.
 	typeNode = type(node).__name__ ;
-	if typeNode == "If" or typeNode=="Switch" or typeNode=="While" or typeNode=="DoWhile":
+	if typeNode == "If":
 		DFS(node.cond);
+		DFS(node.iftrue);
+		DFS(node.iffalse);
 	elif typeNode == "Assignment":
 		print "####WARNING####";  # After the necessary dict check
-	
+	elif typeNode=="Case":
+		DFS(node.expr);
+		for i in node.stmts:
+			DFS(i)
 	elif typeNode=="BinaryOp":
-		#No need for a DFS here? Just check the type.
 		if node.op=='==':
 			print "Fine!"
+		DFS(node.op);
+		DFS(node.left);
+		DFS(node.right);
 	elif typeNode=="FuncDef":
 		for i in node.body:
 			DFS(i);
+	elif typeNode=="Compound":
+		for i in node.block_items:
+			DFS(i);
+	elif typeNode=="CompoundLiteral":
+		DFS(node.init);    #type' won't need a DFS
+	elif typeNode=="Decl":
+		DFS(node.init);
+	elif typeNode=="DeclList":
+		for i in node.decls:
+			DFS(i);
+	elif typeNode=="Default":
+		for i in node.stmts:
+			DFS(i);
+	elif typeNode=="DoWhile":
+		DFS(node.cond);
+		DFS(node.stmt);
+	elif typeNode=="Enum":
+		DFS(node.values);
+	elif typeNode=="Enumerator":
+		DFS(node.value);
+	elif typeNode=="EnumeratorList":
+		for i in node.enumerators:
+			DFS(i);
+	elif typeNode=="ExprList":
+		for i in node.exprs:
+			DFS(i);
+	elif typeNode=="FileAST":
+		for i in node.ext:
+			DFS(i);
+	elif typeNode=="For":
+		DFS(node.init);
+		DFS(node.cond);
+		DFS(node.next);
+		DFS(node.stmt);
+	elif typeNode=="FuncCall":
+		DFS(node.args);  #assuming name won't require a dfs
+	elif typeNode=="FuncDecl":
+		DFS(node.args);
+
+	elif typeNode=="InitList":
+		for i in node.exprs:
+			DFS(i);
+	elif typeNode=="Label":
+		DFS(node.stmt);
+	elif typeNode=="NamedInitializer":
+		for i in node.name:
+			DFS(i);
+		DFS(node.expr);
+	elif typeNode=="ParamList":
+		for i in node.params:
+			DFS(i);
+	elif typeNode=="Return":
+		DFS(node.expr);
+	elif typeNode=="Struct":
+		for i in node.decls:
+			DFS(i);
+	elif typeNode=="TernaryOp":
+		DFS(node.cond);
+		DFS(node.iftrue);
+		DFS(node.iffalse);
+	elif typeNode=="UnaryOp":
+		DFS(node.expr);
+	elif typeNode=="Union":
+		for i in node.decls:
+			DFS(i);
+	elif typeNode=="While":
+		DFS(node.cond);
+		DFS(node.stmt);
+
 
 	
 	
